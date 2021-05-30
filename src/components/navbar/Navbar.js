e19a9ea4a372978c3293ef8a451ddd9a35922dc5
Hajra <hajra.mshahid24@gmail.com>
2021-05-30 18:26:04 +0530

    v.3.3

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,6 +4,18 @@ import './style.css'
 
 const Navbar = () => {
 
+    const [show , setShow] = useState('close');
+
+    const handleMenu = (x) => {
+        setShow('show');
+
+    }
+
+    const close = ()  => {
+        setShow('close');
+
+    }
+
     return (
         <div className="navbar">
             <img className="logo" src={brand} />
@@ -19,13 +31,29 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="phone">
-                    <i class="fas fa-phone-alt"></i>
+                    <i className="fas fa-phone-alt"></i>
                     <h1><span>866-986-3356</span></h1>
                 </div>
             </div>
             
             <div className="sm__nav">
-                <i class="fa fa-bars" aria-hidden="true"></i>
+                <i class="fa fa-bars" onClick={handleMenu}  aria-hidden="true"></i>
+                <div className={`nav ${show}`}>
+                    <i class="fa fa-times" onClick={close} aria-hidden="true"></i>
+                        <ul>
+                            <li onClick={close}>ABOUT</li>
+                            <li onClick={close}>SERVICES</li>
+                            <li onClick={close}>RESOURCES</li>
+                            <li onClick={close}>CAREERS</li>
+                            <li onClick={close}>HOME CARE HEROES</li>
+                            <li onClick={close}>CONTACT</li>
+                        </ul>
+                    <div className="phone">
+                        <i class="fas fa-phone-alt"></i>
+                        <h1><span>866-986-3356</span></h1>
+                    </div>
+                    
+                </div>
             </div>
         </div>
     )
